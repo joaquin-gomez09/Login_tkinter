91aa5edff6ddc868b82c5c9a41bec3de2ed7cb86
Joaquin Cardiel Gomez <joaquin.cardiel05@gmail.com>
2025-08-05 23:53:17 -0300

    imagen

--- imagen.py
+++ imagen.py
@@ -7,7 +7,7 @@ ventana.title("Prueba de imagen")
 
 # Cargar la imagen
 
-imagen = Image.open("app_login/imagenes/candado.png")
+imagen = Image.open("app_login/imagenes/candado_pillow.jpg")
 imagen_tk = ImageTk.PhotoImage(imagen)
 
 # Crear y mostrar el label con imagen
